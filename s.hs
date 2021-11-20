77e95e407865cf97e5d4accb16eabbf43dbf8d78
mbueno-g <buenogarciamarina@gmail.com>
2021-11-20 18:48:40 +0100

    Update Nota [Int] checking rows and columns

--- s.hs
+++ s.hs
@@ -11,6 +11,9 @@ data Celda = Definitivo Int | Provisional Int | Nota [Int]
 type Fila = [Celda]
 type Sudoku = [Fila]
 
+
+-----------------------  READ   ---------------------------
+
 procesa :: IO ()
 procesa = do putStr "Dime el nombre del sudoku: "
              name <- getLine
@@ -25,16 +28,28 @@ readSudoku xs = lines xs
 
 --Crea el tipo Sudoku
 toSudoku :: [String] -> [Fila]
-toSudoku xs = map toFila xs
+toSudoku xs = zipWith toFila xs xs
 
-toFila :: String -> Fila
+{-toFila :: String -> Fila
 toFila xs = map toCelda xs
 
 toCelda :: Char -> Celda
 toCelda x 
         | Data.Char.isDigit x && x > '0' && x < '9' = Definitivo (Data.Char.digitToInt x)
         | otherwise = Nota [1,2,3,4,5,6,7,8,9]
+-}
 
+toFila :: String -> String -> Fila
+toFila [] _ = []
+toFila (x:xs) ys
+         | Data.Char.isDigit x && x >= '1' && x <= Data.Char.intToDigit (length ys) = [Definitivo (Data.Char.digitToInt x)] ++ toFila xs ys
+         | otherwise = [Nota [1..n]] ++ toFila xs ys
+              where n = length ys
+
+
+
+
+-----------------------  UPDATE   ---------------------------
 
 --Elementos definitivos de una Fila
 inRow :: Fila -> [Int]
@@ -42,19 +57,30 @@ inRow [] = []
 inRow ((Definitivo x):xs)  =  [x] ++ inRow xs
 inRow ((Nota ys):xs) = inRow xs
 
---Actualiza el Sudoku
-update :: Sudoku -> Sudoku
-update [] = []
-update (x:xs) = [updateNotaf lista x] ++ update xs
+
+--Actualiza Nota [Int] mirando la fila
+updateNotaf :: Sudoku -> Sudoku
+updateNotaf [] = []
+updateNotaf (x:xs) = [updateFila lista x] ++ updateNotaf xs
                where lista = inRow x
 
+
+-- Actualiza Nota [Int] mirando la columna
+updateNotac :: Sudoku -> Sudoku
+updateNotac xs = updateNotaf (Data.List.transpose xs)
+
+
+-- Actualiza Nota [Int] mirando las cuadrículas
+
+
+
 -- Actualiza Nota [Int] mirando la fila
-updateNotaf :: [Int] -> Fila -> Fila
-updateNotaf xs ((Definitivo e):es)
-                         | es /= [] = [Definitivo e] ++ updateNotaf xs es
+updateFila :: [Int] -> Fila -> Fila
+updateFila xs ((Definitivo e):es)
+                         | es /= [] = [Definitivo e] ++ updateFila xs es
                          | otherwise = [Definitivo e]
-updateNota xs ((Nota ys):es)
-                         | es /= [] = [Nota (eliminaElems xs ys)] ++ updateNotaf xs es
+updateFila xs ((Nota ys):es)
+                         | es /= [] = [Nota (eliminaElems xs ys)] ++ updateFila xs es
                          | otherwise = [Nota (eliminaElems xs ys)]
 
 --Elimina todos los elementos de una lista de otra lista
@@ -65,7 +91,30 @@ eliminaElems (x:xs) ys
         | otherwise = eliminaElems xs ys
 
 
--- Muestra los Definitvo Int del Sudoku
+
+
+
+-----------------------   FILL SUDOKU   ---------------------------
+
+
+-- Convierte en definitivo el único elemento de la lista Nota [Int]
+definitivo :: Sudoku -> Sudoku
+definitivo xs = map toDefinitivo xs
+
+toDefinitivo:: Fila -> Fila
+toDefinitivo [] = []
+toDefinitivo ((Definitivo x):xs) = [Definitivo x] ++ toDefinitivo xs
+toDefinitivo ((Nota x):xs)
+              | length (x) == 1 = [Definitivo (x !! 0)] ++ toDefinitivo xs
+              | otherwise = [Nota x] ++ toDefinitivo xs
+
+
+
+
+
+-----------------------   SHOW   ---------------------------
+
+-- Muestra los DefinitIvo Int del Sudoku
 showSudoku :: Sudoku -> String
 showSudoku = unlines . map (unwords . map showCelda)
   where
