6486e0dea7520078b08cac7b8f825e833a578042
mbueno-g <buenogarciamarina@gmail.com>
2021-12-17 21:51:37 +0100

    Deleted

--- prueba.hs
+++
@@ -1,8 +0,0 @@
-procesa :: IO ()
-procesa = do putStr "Nombre del sudoku: "
-             nombre <- getLine
-             contenido <- readFile nombre
-             putStr contenido
-						 --let numeros :: [Int]
-               --  numeros = map read (words contenido)
-             --print (qsort numeros)
